fab1cd6a666a1e70f7821bdf3804288e6001b7c6
huytoan245 <41029010+huytoan245@users.noreply.github.com>
2026-09-09 11:16:07 +0700

    v2.7.0: fix generated csproj PDFsharp newline

--- tach-xep-pdf-v2.3.2/prepare_build_v270.py
+++ tach-xep-pdf-v2.3.2/prepare_build_v270.py
@@ -18,7 +18,7 @@ s = s.replace('release-v2.6.1', 'release-v2.7.0').replace('v2.6.1', 'v2.7.0').re
 project_write = "Set-Content (Join-Path $Project 'TachXepTrangPDF.csproj') $proj -Encoding UTF8"
 if project_write not in s:
     raise SystemExit('generated csproj write marker missing')
-pdfsharp_line = "$proj = $proj.Replace('<PackageReference Include=\"Microsoft.ML.OnnxRuntime\" Version=\"1.22.1\" />','<PackageReference Include=\"Microsoft.ML.OnnxRuntime\" Version=\"1.22.1\" />`r`n    <PackageReference Include=\"PDFsharp\" Version=\"6.2.4\" />')"
+pdfsharp_line = "$proj = $proj.Replace('<PackageReference Include=\"Microsoft.ML.OnnxRuntime\" Version=\"1.22.1\" />', '<PackageReference Include=\"Microsoft.ML.OnnxRuntime\" Version=\"1.22.1\" />' + [Environment]::NewLine + '    <PackageReference Include=\"PDFsharp\" Version=\"6.2.4\" />')"
 s = s.replace(project_write, pdfsharp_line + "\n" + project_write, 1)
 
 old_req = "'PreviewPanMouseDown','PreviewPanMouseMove','SetPreviewPanPosition','CanPanPreview','AutoScroll = true','AutoScrollMinSize','Xoay trái 90°'"
